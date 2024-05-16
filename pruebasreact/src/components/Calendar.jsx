7f905ddcfefea585ea052d467cad4b925b0b8217
ErmacUwU <al20760334@ite.edu.mx>
2024-05-15 20:55:43 -0700

    15 de Mayo de 2024

---
+++ pruebasreact/src/components/Calendar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid' 
+import timeGridPlugin from '@fullcalendar/timegrid' 
+import interactionPlugin from "@fullcalendar/interaction" 
+const Calendar = () => {
+  return (
+    <div>
+        <FullCalendar
+        plugins={[dayGridPlugin,timeGridPlugin,interactionPlugin]}
+        initialView={"timeGridWeek"}
+        />
+    </div>
+  )
+}
+
+export default Calendar
