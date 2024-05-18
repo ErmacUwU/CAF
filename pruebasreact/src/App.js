cb6ed19af225002337770a709611f5d0e52283cb
ErmacUwU <al20760334@ite.edu.mx>
2024-05-17 21:47:11 -0700

    Termine todos los meses

--- pruebasreact/src/App.js
+++ pruebasreact/src/App.js
@@ -3,7 +3,15 @@ import Marzo from "./components/Marzo";
 import Calendar from "./components/Calendar";
 import Enero from "./components/Enero";
 import Febrero from "./components/Febrero";
-
+import Mayo from './components/Mayo';
+import Junio from './components/Junio';
+import Julio from './components/Julio';
+import Agosto from './components/Agosto';
+import Septiembre from './components/Septiembre';
+import Octubre from './components/Octubre';
+import Noviembre from './components/Noviembre';
+import Diciembre from './components/Diciembre';
+import './components/App.css';
 function App() {
   return (
     <div className="App">
@@ -12,6 +20,14 @@ function App() {
       <Febrero/>
       <Marzo/>
       <Abril/>
+      <Mayo/>
+      <Junio />
+      <Julio />
+      <Agosto />
+      <Septiembre />
+      <Octubre />
+      <Noviembre />
+      <Diciembre />
     </div>
   );
 }
