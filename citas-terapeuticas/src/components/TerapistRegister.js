6b60d0f426335bb26fc73cdceb42024091576342
ErmacUwU <al20760334@ite.edu.mx>
2024-05-22 20:47:33 -0700

    22 de Mayo de 2024

--- citas-terapeuticas/src/components/TerapistRegister.js
+++ citas-terapeuticas/src/components/TerapistRegister.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import '../components/TerapistRegister.css';
 
 const TerapistRegister = () => {
   return (
-    <div>
-        
-    </div>
+   <form>
+    <label for="nombre">Nombre</label>
+    <input type="email"/>
+    <label for="apellidos">Apellidos</label>
+    <input type="apellidos"/>
+    <label for="nacimiento">Fecha de Nacimiento</label>
+    <input type="date"/>
+    <label for="sexo">Sexo</label>
+    <select>
+      <option value="masculino">Masculino</option>
+      <option value="femenino">Femenino</option>
+    </select>
+   </form>
   )
 }
 
